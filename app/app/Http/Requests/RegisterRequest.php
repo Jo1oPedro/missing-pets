7b763bd76704f405b27013b824065a2f2c48e57e
Jo1oPedro <joao.pedreira@estudante.ufjf.br>
2024-03-15 09:45:00 -0300

    adiciona registerController e registerRequest

---
+++ app/app/Http/Requests/RegisterRequest.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+class RegisterRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     */
+    public function authorize(): bool
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array<string, \Illuminate\Contracts\Validation\ValidationRule|array<mixed>|string>
+     */
+    public function rules(): array
+    {
+        return [
+            'name' => 'required|string',
+            'email' => 'required|string|email|unique:users',
+            'password' => 'required|string|confirmed',
+            'password_confirmation' => 'required|string'
+        ];
+    }
+
+    public function messages()
+    {
+        return [
+            'required' => 'O campo :attribute é obrigatório.',
+            'string' => 'O campo :attribute precisa ser um texto válido.',
+            'password.confirmed' => 'As senhas não são correspondentes.',
+            'unique' => 'O atributo :attribute já está em uso.',
+            'email' => 'O campo :attribute precisa ser um e-mail válido.',
+        ];
+    }
+}
