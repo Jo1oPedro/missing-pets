391d09c033955a072ff6d8504d59c51e0eafca43
jo1oPedro <joao.pedreira@estudante.ufjf.br>
2024-03-25 16:25:35 -0300

    retira zend_extension do php.ini

--- app/tests/Feature/PetPostControllerTest.php
+++ app/tests/Feature/PetPostControllerTest.php
@@ -2,6 +2,7 @@
 
 namespace Tests\Feature;
 
+use App\Models\User;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 use Illuminate\Foundation\Testing\WithFaker;
 use Tests\TestCase;
@@ -11,10 +12,34 @@ class PetPostControllerTest extends TestCase
     /**
      * A basic feature test example.
      */
-    public function test_example(): void
+    public function get_pet_posts_paginated_sucessfully(): void
     {
-        $response = $this->get('/');
-
-        $response->assertStatus(200);
+        $user = User::factory()->create();
+        $this->actingAs($user);
+        $response = $this->get('/api/pet/posts');
+        $response->assertStatus(200)
+            ->assertJsonStructure(
+                [
+                    "current_page",
+                    "data",
+                    "first_page_url",
+                    "from",
+                    "last_page",
+                    "last_page_url",
+                    "links" => [
+                        "*" => [
+                            "url",
+                            "label",
+                            "active",
+                        ],
+                    ],
+                    "next_page_url",
+                    "path",
+                    "per_page",
+                    "prev_page_url",
+                    "to",
+                    "total",
+                ]
+            );
     }
 }
