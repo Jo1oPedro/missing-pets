697de7a69bc9009c038c447a8442bb78b7ffaa07
jo1oPedro <joao.pedreira@estudante.ufjf.br>
2024-03-25 18:01:29 -0300

    adiciona form request para criação de posts de pets

---
+++ app/app/Http/Requests/PetPostRequest.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+class PetPostRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     */
+    public function authorize(): bool
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array<string, \Illuminate\Contracts\Validation\ValidationRule|array<mixed>|string>
+     */
+    public function rules(): array
+    {
+        return [
+            'user_id' => 'required|exists:users,id',
+            'coordinate_x' => 'required|integer',
+            'coordinate_y' => 'required|integer',
+            'breed' => 'required|string',
+            'type' => 'required|string',
+            'additional_info' => 'sometimes|string',
+        ];
+    }
+
+    public function messages()
+    {
+        return [
+            'required' => 'O campo :attribute é obrigatório.',
+            'exists' => 'Usuário inexistente.',
+            'integer' => 'O campo :attribute precisa ser um inteiro valido.',
+            'string' => 'O campo :attribute precisa ser um texto válido.',
+        ];
+    }
+}
