0fa05af9682a585a6054202970acba0ad38b7fe8
jo1oPedro <joao.pedreira@estudante.ufjf.br>
2024-04-02 20:16:23 -0300

    adiciona tabela com fotos de pets para os posts

---
+++ app/app/Models/PetPostPhoto.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class PetPostPhoto extends Model
+{
+    protected $guarded = [];
+
+    use HasFactory;
+}
