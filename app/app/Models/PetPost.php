23eadb92b00ece22c120ecccaed02bf54a156197
jo1oPedro <joao.pedreira@estudante.ufjf.br>
2024-04-07 16:36:12 -0300

    adiciona métodos para alterar campos para datetimes na model de PetPost

--- app/app/Models/PetPost.php
+++ app/app/Models/PetPost.php
@@ -2,6 +2,7 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Casts\Attribute;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
@@ -21,5 +22,19 @@ class PetPost extends Model
     public function pet_post_images(): HasMany
     {
         return $this->hasMany(PetPostImage::class);
+    }
+
+    protected function createdAt(): Attribute
+    {
+        return Attribute::make(function (string $value) {
+            return new \DateTime($value);
+        });
+    }
+
+    protected function updatedAt(): Attribute
+    {
+        return Attribute::make(function (string $value) {
+            return new \DateTime($value);
+        });
     }
 }
